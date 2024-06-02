05e59539556b7deb64c102dab9b883dd0ccaca02
Siddharth Mohanty <120799968+Siddharth-Mohanty-308@users.noreply.github.com>
2024-06-02 14:48:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,16 +10,16 @@ model=joblib.load('decision_tree_model.joblib')
 st.title("Credit Card Approval Prediction")
 
 with st.form('Details Form'):
-    reports=st.number_input("Number of Reports:", min_value=0, step=1)
+    reports=st.number_input("Number of Reports:", min_value=0, step=1, format='%d')
     age=st.slider("Age:", min_value=18, max_value=83, step=1)
-    income=st.number_input("Yearly Income (in$):", min_value=0, step=10000)
-    expenditure=st.number_input("Monthly Card Expenditure in($):", min_value=0, step=100)
+    income=st.number_input("Yearly Income (in$):", min_value=0, step=10000, format='%d')
+    expenditure=st.number_input("Monthly Card Expenditure in($):", min_value=0, step=100, format='%d')
     house_owner=st.selectbox('Do you own a House:',options=[(1,'Yes'),(0,'No')], format_func=lambda x: x[1])[0]
     self_emp=st.selectbox('Are you Self Employed:',options=[(1,'Yes'),(0,'No')], format_func=lambda x: x[1])[0]
-    dependents=st.number_input('Number of Dependents:', min_value=0, step=1)
-    curr_add=st.number_input('Months Lived in Current Address:', min_value=0, step=1)    
-    major_cards=st.number_input('Number of Major Credit Cards Held:', min_value=0, step=1)
-    active_accounts=st.number_input('Number of Active Credit Accounts', min_value=0, step=1)
+    dependents=st.number_input('Number of Dependents:', min_value=0, step=1, format='%d')
+    curr_add=st.number_input('Months Lived in Current Address:', min_value=0, step=1, format='%d')    
+    major_cards=st.number_input('Number of Major Credit Cards Held:', min_value=0, step=1, format='%d')
+    active_accounts=st.number_input('Number of Active Credit Accounts', min_value=0, step=1, format='%d')
 
     submitted=st.form_submit_button('Submit')
 
