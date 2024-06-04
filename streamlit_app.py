92a2fc17fd8c8102aad0f79542884fe014139e8f
Siddharth Mohanty <120799968+Siddharth-Mohanty-308@users.noreply.github.com>
2024-06-04 21:31:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,10 @@ st.caption('Made by Siddharth Mohanty')
 
 with st.form('credit_form'):
     st.header('Enter your details:')
-    reports = st.number_input('Number of Reports', min_value=0, step=1, format='%d')
-    age = st.slider('Age', min_value=18, max_value=95, step=1)
     income = st.number_input('Yearly Income', min_value=0, step=1000, format='%d')
     expenses = st.number_input('Monthly Card Expenses', min_value=0, step=100, format='%d')
+    age = st.slider('Age', min_value=18, max_value=95, step=1)
+    reports = st.number_input('Number of Reports', min_value=0, step=1, format='%d')
     house_owner = st.selectbox('Owner of House', options=[(1, 'Yes'), (0, 'No')], format_func=lambda x: x[1])[0]
     self_employed = st.selectbox('Self Employed', options=[(1, 'Yes'), (0, 'No')], format_func=lambda x: x[1])[0]
     dependents = st.number_input('Number of Dependents', min_value=0, step=1, format='%d')
@@ -36,9 +36,9 @@ if submitted:
             prediction = model.predict(features)[0]
             
             if prediction == 1:
-                result = "Credit Card Approved"
+                result = "Result: Credit Card Approved"
             else:
-                result = "Credit Card Not Approved"
+                result = "Result: Credit Card Not Approved"
             
             st.subheader(result)
 
