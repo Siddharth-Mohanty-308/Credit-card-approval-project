c1fce916409a1f88d5d4b758235a8592b2158001
Siddharth Mohanty <120799968+Siddharth-Mohanty-308@users.noreply.github.com>
2024-06-13 21:47:44 +0530

    Update 3_contact.py

--- pages/3_contact.py
+++ pages/3_contact.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
+col1, col2 = st.columns(2,3)
 
-# st.image("my_image.png", caption="This is an example image", use_column_width=True)
-st.markdown("### Siddharth Mohanty")
-st.markdown("[My LinkedIn id](https://www.linkedin.com/in/siddharth6791/)")
-st.markdown("[My email](siddharth6791@gmail.com)")
+with col1:
+  st.header("Column 1")
+  st.write("Add image")
+  st.markdown("[My LinkedIn id](https://www.linkedin.com/in/siddharth6791/)")
+  st.markdown("[My email](siddharth6791@gmail.com)")
+
+with col2:
+  st.header("Column 2")
+  st.markdown("### Siddharth Mohanty")
+  st.write("Add info about me section")
+  # st.image("my_image.png", caption="This is an example image", use_column_width=True)
