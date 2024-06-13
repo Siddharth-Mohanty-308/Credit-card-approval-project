ceceeb2ef1454f6ff17435f4ef2ee336d69711ab
Siddharth Mohanty <120799968+Siddharth-Mohanty-308@users.noreply.github.com>
2024-06-13 21:30:25 +0530

    Update 3_contact.py

--- pages/3_contact.py
+++ pages/3_contact.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
-st.image("my_image.png", caption="This is an example image", use_column_width=True)
+# st.image("my_image.png", caption="This is an example image", use_column_width=True)
 st.markdown("### Siddharth Mohanty")
 st.markdown("[My LinkedIn id](www.linkedin.com/in/siddharth6791)")
-st.markdown("### Add linkedin link ")
-st.markdown("### Add email ")
+st.markdown("### email: siddharth6791@gmail.com ")
