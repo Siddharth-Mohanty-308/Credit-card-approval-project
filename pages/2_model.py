8975a6704802f3e4beb04248c00b8d93f115ecde
Siddharth Mohanty <120799968+Siddharth-Mohanty-308@users.noreply.github.com>
2024-06-14 20:01:23 +0530

    Update 2_model.py

--- pages/2_model.py
+++ pages/2_model.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import joblib
+import scikit-learn 
 
+model = joblib.load('decision_tree_model.joblib')
 st.markdown("### Model metrics")
+# st.image()
 st.markdown("### Model diagram")
+
+dot_data=tree.export_graphviz(
+    model,
+    out_file=None
+)
+
+st.graphviz_chart(dot_data)
