0d8001c0b36ba8fffb889c5317dc356b6a980caf
Siddharth Mohanty <120799968+Siddharth-Mohanty-308@users.noreply.github.com>
2024-06-14 20:05:26 +0530

    Update 3_contact.py

--- pages/3_contact.py
+++ pages/3_contact.py
@@ -4,12 +4,15 @@ col1, col2 = st.columns([2,3])
 
 with col1:
   st.header("Column 1")
-  st.write("Add image")
+  st.image("https://github.com/Siddharth-Mohanty-308/Credit-card-approval-project/blob/main/pages/my_image.png", use_column_width=True)
   st.markdown("[My LinkedIn id](https://www.linkedin.com/in/siddharth6791/)")
-  st.markdown("[My email](siddharth6791@gmail.com)")
+  st.markdown("email: siddharth6791@gmail.com")
 
 with col2:
   st.header("Column 2")
   st.markdown("### Siddharth Mohanty")
-  st.write("Add info about me section")
-  # st.image("my_image.png", caption="This is an example image", use_column_width=True)
+  st.write
+  (""" I am a recent Computer Science graduate with strong programming skills in Python, specializing in data science and visualization have 
+  equipped me with practical expertise in machine learning, data analysis, and software development. 
+  I am passionate about leveraging my technical skills to solve complex problems and drive innovation. 
+  """)
