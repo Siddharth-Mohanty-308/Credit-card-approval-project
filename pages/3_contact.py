2cd007e48d598cd9671b2b00fcea4ec5abee8533
Siddharth Mohanty <120799968+Siddharth-Mohanty-308@users.noreply.github.com>
2024-06-21 19:54:41 +0530

    Update 3_contact.py

--- pages/3_contact.py
+++ pages/3_contact.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 image_url = 'https://raw.githubusercontent.com/Siddharth-Mohanty-308/Credit-card-approval-project/main/pages/my_image.png'
-col1, col2 = st.columns([2, 3])
+col1, col2 = st.columns([1, 3])
 
 with col1:
-  st.image(image_url, use_column_width=True)
+  st.image(image_url, use_column_width=False)
   st.markdown("[My LinkedIn id](https://www.linkedin.com/in/siddharth6791/)")
   st.markdown("email: siddharth6791@gmail.com")
 
 with col2:
   st.header("Siddharth Mohanty")
-  st.write("I am a recent Computer Science graduate with strong programming skills in Python, specializing in data science and visualization have equipped me with practical expertise in machine learning, data analysis, and software development.") 
+  st.write("I am a recent Computer Science graduate with strong programming skills in Python, specializing in data science and visualization have equipped me with practical knowledge in machine learning, data analysis, and software development.") 
   st.write("I am passionate about leveraging my technical skills to solve complex problems and drive innovation.")
