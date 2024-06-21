e0a6fab0276f4ef0f4585eb648410c4cb1a6cde0
Siddharth Mohanty <120799968+Siddharth-Mohanty-308@users.noreply.github.com>
2024-06-21 20:05:13 +0530

    Update 2_model.py

--- pages/2_model.py
+++ pages/2_model.py
@@ -3,8 +3,8 @@ import joblib
 from sklearn import tree
 
 model = joblib.load('decision_tree_model.joblib')
-image_url="https://raw.githubusercontent.com/Siddharth-Mohanty-308/Credit-card-approval-project/main/readme_assets/conf_matrix_final.png
-"
+image_url="https://raw.githubusercontent.com/Siddharth-Mohanty-308/Credit-card-approval-project/main/readme_assets/conf_matrix_final.png"
+
 st.markdown("### Model metrics")
 st.caption("Confusion Matrix")
 st.image(image_url)
